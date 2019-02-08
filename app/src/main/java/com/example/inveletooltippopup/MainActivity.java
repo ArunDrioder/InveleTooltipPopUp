b3f79e1a1ac8f61645f1061b4c80cdbfa086ecd7
ninos <arunprasadh.s@gmail.com>
2019-02-08 10:24:54 +0530

    Initial commit for InveleTooltipPopUp. Need to check on how to create custom tooltip with image

---
+++ app/src/main/java/com/example/inveletooltippopup/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.inveletooltippopup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import it.sephiroth.android.library.tooltip.Tooltip;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void bottomToolTipDialogBox(View view) {
+        Button toolTipShowButton = (Button) findViewById(R.id.button_tooltip_bottom);
+        Tooltip.make(this,
+                new Tooltip.Builder(101)
+                        .anchor(toolTipShowButton, Tooltip.Gravity.BOTTOM)
+                        .closePolicy(new Tooltip.ClosePolicy()
+                                .insidePolicy(true, false)
+                                .outsidePolicy(true, false), 4000)
+                        .activateDelay(900)
+                        .showDelay(400)
+                        .text("Android PopupWindow with Tooltip Arrow Below Button or view or layout")
+                        .maxWidth(600)
+                        .withArrow(true)
+                        .withOverlay(true).build()
+        ).show();
+    }
+
+    public void rightToolTipPopupWindow(View view) {
+        Button toolTipShowButton = (Button) findViewById(R.id.button_tooltip_right_side);
+        Tooltip.make(this,
+                new Tooltip.Builder(101)
+                        .anchor(toolTipShowButton, Tooltip.Gravity.RIGHT)
+                        .closePolicy(new Tooltip.ClosePolicy()
+                                .insidePolicy(true, false)
+                                .outsidePolicy(true, false), 2000)
+                        .activateDelay(900)
+                        .showDelay(400)
+                        .text("Android PopupWindow with Tooltip Arrow right side of button or view or layout")
+                        .maxWidth(600)
+                        .withArrow(true)
+                        .withOverlay(true).build()
+        ).show();
+    }
+
+    public void leftToolTipPopupWindow(View view) {
+        Button toolTipShowButton = (Button) findViewById(R.id.button_tooltip_left_side);
+        Tooltip.make(this,
+                new Tooltip.Builder(101)
+                        .anchor(toolTipShowButton, Tooltip.Gravity.TOP)
+                        .closePolicy(new Tooltip.ClosePolicy()
+                                .insidePolicy(true, false)
+                                .outsidePolicy(true, false), 4000)
+                        .activateDelay(700)
+                        .showDelay(200)
+                        .text("Android PopupWindow with Tooltip Arrow left side of button or view or layout")
+                        .maxWidth(600)
+                        .withArrow(true)
+                        .withOverlay(true).build()
+        ).show();
+    }
+}
